c569ed667e7f028893d401e5855fc2a025dc145e
FreezB11 <yashrajrao4@gmail.com>
2024-10-15 19:16:25 +0530

    basic needs setup

---
+++ src/utils.h
@@ -0,0 +1,15 @@
+/*
+    this aint ur normal utils
+    just to reduce a lot of typings
+*/
+#ifndef _UTILS_H
+#define _UTILS_H
+
+// int = 4 bytes = 32 bits
+// char = 1 byte = 8 bits
+typedef char            int8;
+typedef short           int16;
+typedef int             int32;
+typedef long long       int64;
+
+#endif
