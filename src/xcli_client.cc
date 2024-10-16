f9458f66bd9026cff7d02843b25764f8df36a021
FreezB11 <yashrajrao4@gmail.com>
2024-10-16 08:57:32 +0530

    minor change

---
+++ src/xcli_client.cc
@@ -0,0 +1,15 @@
+/*
+    @author: hsay
+    
+    the main client side code..
+*/
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+int main(){
+    
+}
