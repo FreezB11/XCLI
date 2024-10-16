e38f2be77ce7419ca401cb73b69bef0720cde555
FreezB11 <yashrajrao4@gmail.com>
2024-10-16 08:34:21 +0530

    minor change

--- src/utils.h
+++ src/utils.h
@@ -5,11 +5,20 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
+#include <ctime>
+#include <chrono>
+#include <iostream>
 // int = 4 bytes = 32 bits
 // char = 1 byte = 8 bits
 typedef char            int8;
 typedef short           int16;
 typedef int             int32;
 typedef long long       int64;
+
+
+// LOGGING
+ #define log(x) std::cout << x << std::endl;
+ #define tlog(x)
+
 
 #endif
