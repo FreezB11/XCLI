67fce1bf7695420dbebde62ebd86b2ac9286f12a
FreezB11 <yashrajrao4@gmail.com>
2024-10-17 20:00:37 +0530

    slapping the sockets baby

--- src/xcli_server.cc
+++ src/xcli_server.cc
@@ -13,7 +13,7 @@
 #define TEMP_port 8080
 
 int main(){
-    int client_sock;
+    int client_sock[5];
     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
     const char* msg;
 
@@ -24,11 +24,18 @@ int main(){
     server_addr.sin_port = htons(TEMP_port);
 
     bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-    listen(server_sock, 5);
-    client_sock = accept(server_sock, nullptr, nullptr);
     char buffer[1024] = {0};
-    recv(client_sock, buffer, sizeof(buffer),0);
+    int i = 0;
+    while(true){
+        listen(server_sock, 5);
+        client_sock[i] = accept(server_sock, nullptr, nullptr);
+        if(recv(client_sock[i], buffer, sizeof(buffer),0) > 0 ){
+            std::cout << buffer << std::endl;
+            i++;
+        }else{
+            break;
+        }
+    }
+    std::cout << "so we are here" << std::endl;
 
-    std::cout << buffer << std::endl;
 }
